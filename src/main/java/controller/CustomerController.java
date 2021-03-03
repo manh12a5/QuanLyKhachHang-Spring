956b4a37dd2b8cd9aee3391f7c75d3374888f9bd
manh12a5 <73921743+manh12a5@users.noreply.github.com>
2021-03-03 14:47:21 +0700

    First commit

---
+++ src/main/java/controller/CustomerController.java
@@ -0,0 +1,71 @@
+package controller;
+
+import model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import service.customer.CustomerService;
+import service.customer.ICustomerService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @Autowired
+    private ICustomerService customerService;
+
+    //Show All
+    @GetMapping("")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        List<Customer> customers = customerService.findAll();
+        modelAndView.addObject("customers",customers);
+        return modelAndView;
+
+    }
+
+    //Create
+    @GetMapping("/create")
+    public ModelAndView showFormCreate() {
+        ModelAndView modelAndView = new ModelAndView("create");
+        modelAndView.addObject("create", new Customer());
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public ModelAndView createCustomer(@ModelAttribute Customer customer) {
+        int id = customerService.findAll().size();
+        customer.setId(id);
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("create");
+        modelAndView.addObject("create", new Customer());
+        return modelAndView;
+    }
+
+    //Edit
+    @GetMapping("/edit/{id}")
+    public ModelAndView showFormEdit(@PathVariable int id) {
+        ModelAndView modelAndView = new ModelAndView("edit");
+        Customer customer = customerService.findById(id);
+        modelAndView.addObject("edit", customer);
+        return modelAndView;
+    }
+
+    @PostMapping("/edit/{id}")
+    public ModelAndView editCustomer(@PathVariable int id, Customer customer) {
+        customer.setId(id);
+        customerService.update(id, customer);
+        return new ModelAndView("edit", "edit", customerService.findAll());
+    }
+
+    //Delete
+    @GetMapping("/delete/{id}")
+    public ModelAndView deleteCustomer(@PathVariable int id) {
+        customerService.remove(id);
+        return new ModelAndView("redirect:/customers");
+    }
+
+}
---
+++ src/main/java/service/customer/CustomerService.java
@@ -0,0 +1,47 @@
+package service.customer;
+
+import model.Customer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CustomerService implements ICustomerService {
+
+    private static Map<Integer, Customer> customers;
+
+    static {
+        customers = new HashMap<>();
+        customers.put(1, new Customer(1, "1", "1@gmail.com", "Ha Noi"));
+        customers.put(2, new Customer(2, "2", "2@gmail.com", "London"));
+        customers.put(3, new Customer(3, "3", "3@gmail.com", "Ho Chi Minh"));
+        customers.put(4, new Customer(4, "4", "4@gmail.com", "New York"));
+        customers.put(5, new Customer(5, "5", "5@gmail.com", "Dubai"));
+    }
+    @Override
+    public List<Customer> findAll() {
+        return new ArrayList<>(customers.values());
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customers.put(customer.getId(), customer);
+    }
+
+    @Override
+    public Customer findById(int id) {
+        return customers.get(id);
+    }
+
+    @Override
+    public void update(int id, Customer customer) {
+        customers.put(id, customer);
+    }
+
+    @Override
+    public void remove(int id) {
+        customers.remove(id);
+    }
+
+}
